43fc6859b7fb32a456446078da7c38755c8278c3
pablo brocal <pablo.brocal@banza.com.ar>
2024-06-10 16:59:37 -0300

    feature(pypi): publish

--- src/pkg_support_resistance/__init__.py
+++ src/pkg_support_resistance/__init__.py
@@ -5,4 +5,4 @@ from .vanilla_algo import VanillaSupportResistance  # noqa: F401
 # Exports Data-Set
 from .data_set.data_extraction import sr_input_example
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
