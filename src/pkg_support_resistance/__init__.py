79661c977ade419de7d3f6bc3df32d2b5f086e7d
pablo brocal <pablo.brocal@banza.com.ar>
2024-06-11 15:21:41 -0300

    feature(version): update

--- src/pkg_support_resistance/__init__.py
+++ src/pkg_support_resistance/__init__.py
@@ -2,8 +2,9 @@
 
 from .vanilla.vanilla_algo import VanillaSupportResistance  # noqa: F401
 from .kmeans.kmeans_algo import KMeansSupportResistance  # noqa: F401
+from .dbscan.dbscan_algo import DBSCANSupportResistance  # noqa: F401
 
 # Exports Data-Set
 from .data_set.data_extraction import sr_input_example, open_json_file
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
